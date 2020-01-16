a49824d92233467ce724aa98e527a2a1fc280262
Robert Swoboda <rmmswoboda@gmail.com>
2020-01-16 14:43:16 +0100

    clear

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: null
+    }
+  }
+  componentDidMount() {
+    fetch('https://api.spotify.com')
+    .then(response => response.json())
+    .then(data => this.setState({data}))
+    console.log(this.state.data)
+  }
+
+  render () {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
 }
 
 export default App;
